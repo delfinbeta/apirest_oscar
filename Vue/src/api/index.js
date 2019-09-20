ba2f940694bf46cbf4aaeecd9164639523dc3cb8
Oscar Ruiz <23oscarruiz07@gmail.com>
2019-09-20 08:49:18 -0400

    cliente en VueJS listo

--- Vue/src/api/index.js
+++ Vue/src/api/index.js
@@ -1,8 +1,35 @@
 import axios from 'axios'
-export default function getCategories()
-{
-	return axios.get('http://127.0.0.1:8000/api/categories/')
-				.then(function (response){
-					return response.data
-				})
+
+
+const API_URL = 'http://bitacoraplayera.com/api'
+export default class ApiService{
+constructor(){
+}
+
+	// get all categories
+	getCategories(){
+		const url = `${API_URL}/categories`
+		return axios.get(url).then(response => response.data)
+	}
+
+	//get a especific category
+	getCategory(id){
+		const url = `${API_URL}/categories/${id}`
+		return axios.get(url).then(response => response.data)
+	}
+
+	// create category
+	createCategory(obj){
+		const url = `${API_URL}/categories/create`
+		return axios.post(url,{
+			'name': obj.name,
+			'description': obj.description
+		}).then(response => response.data)
+	}
+
+	// delete category
+	deleteCategory(id){
+	    const url = `${API_URL}/api/category/${id}/trash`;
+	    return axios.patch(url);
+	}
 }
